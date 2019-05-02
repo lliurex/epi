301b236b09859dbb0c090bda5b8dc1fe8f43ed5f
jrpelegrina <juapesai@hotmail.com>
2019-05-02 14:39:49 +0200

    WIP in support installation local deb

--- epi-gtk/python3-epigtk/DependsBox.py
+++ epi-gtk/python3-epigtk/DependsBox.py
@@ -11,7 +11,7 @@ import gettext
 gettext.textdomain(settings.TEXT_DOMAIN)
 _ = gettext.gettext
 
-class LoadingBox(Gtk.VBox):
+class DependsBox(Gtk.VBox):
 	
 	def __init__(self):
 		
@@ -28,10 +28,26 @@ class LoadingBox(Gtk.VBox):
 
 		self.css_file=self.core.rsrc_dir+"epi-gtk.css"
 
-		self.main_box=builder.get_object("loading_box")
-		self.loading_label=builder.get_object("loading_label")
-		self.loading_spinner=builder.get_object("loading_spinner")
+		self.main_box=builder.get_object("deb_depends_box")
+		self.deb_depends_label=builder.get_object("deb_depends_label")
 
+		self.scrolledwindow_deb=builder.get_object("scrolledwindow_deb")
+		self.viewport_deb=builder.get_object("viewport_deb")
+
+		self.depends_label=builder.get_object("depends_label")
+		self.depends_label.set_margin_right(10)
+		self.depends_label.set_alignment(0,1)
+		self.depends_label.set_justify(Gtk.Justification.FILL)
+		self.depends_label.set_line_wrap(True)
+		self.depends_label.set_valign(Gtk.Align.START)
+		self.depends_label.set_halign(Gtk.Align.FILL)
+
+
+		self.scrolledwindow_deb.set_shadow_type(Gtk.ShadowType.NONE)
+		self.scrolledwindow_deb.set_overlay_scrolling(True)
+		self.scrolledwindow_deb.set_kinetic_scrolling(True)
+
+		self.scrolledwindow_deb.set_size_request(0,100)
 		
 		self.pack_start(self.main_box,True,True,0)
 
@@ -51,8 +67,7 @@ class LoadingBox(Gtk.VBox):
 		self.style_provider.load_from_file(f)
 
 		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),self.style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-		self.loading_label.set_name("MSG_LABEL")
-				
+			
 			
 	#def set-css_info
 	
@@ -60,6 +75,6 @@ class LoadingBox(Gtk.VBox):
 	
 
 		
-#class LoadingBox
+#class DependsBox
 
 from . import Core
