6355fef6d8295097faaf93beeb0c546db4421723
Juan Ramon Pelegrina <juapesai@hotmail.com>
2021-05-28 14:46:17 +0200

    Changes to improve feedback

--- epi-gtk/python3-epigtk/EpiBox.py
+++ epi-gtk/python3-epigtk/EpiBox.py
@@ -61,12 +61,6 @@ class EpiBox(Gtk.VBox):
 		self.epi_depend_label=builder.get_object("epi_depend_label")
 		self.view_terminal_btn=builder.get_object("view_terminal_btn")
 		self.view_terminal_btn.connect("clicked",self.view_terminal)
-		self.feedback_box=builder.get_object("feedback_box")
-		self.feedback_ok_img=builder.get_object("feedback_ok_img")
-		self.feedback_error_img=builder.get_object("feedback_error_img")
-		self.feedback_info_img=builder.get_object("feedback_info_img")
-		self.feedback_label=builder.get_object("feedback_label")
-		self.feedback_label.set_halign(Gtk.Align.CENTER)
 		self.monitoring=True
 		self.show_terminal=False
 
@@ -93,7 +87,6 @@ class EpiBox(Gtk.VBox):
 		#self.terminal_label.set_name("MSG_LABEL")
 		self.epi_depend_label.set_name("DEPEND_LABEL")
 		self.search_entry.set_name("CUSTOM-ENTRY")
-		self.feedback_label.set_name("MSG_LABEL")
 		self.epi_list_box.set_name("LIST_BOX")
 				
 	#def set_css_info			
@@ -128,7 +121,7 @@ class EpiBox(Gtk.VBox):
 				if info[item]["selection_enabled"]["active"]:
 					self.search_entry.show()
 					self.select_pkg_btn.set_visible(True)
-					self.core.mainWindow.main_window.resize(675,500)
+					self.core.mainWindow.main_window.resize(675,570)
 
 					show_cb=True
 					if info[item]["selection_enabled"]["all_selected"]:
@@ -694,37 +687,7 @@ class EpiBox(Gtk.VBox):
 		os.system(self.launch_cmd)
 
 	#def launch_app	
-	
-	def manage_feedback_box(self,hide,error,info):
-
-		if hide:
-			self.feedback_box.set_name("HIDE_BOX")
-			self.feedback_ok_img.hide()
-			self.feedback_error_img.hide()
-			self.feedback_info_img.hide()
-			self.feedback_label.set_halign(Gtk.Align.CENTER)
-		else:
-			self.feedback_label.set_halign(Gtk.Align.START)
-		
-			if error:
-				self.feedback_box.set_name("ERROR_BOX")
-				self.feedback_ok_img.hide()
-				self.feedback_error_img.show()
-				self.feedback_info_img.hide()
-			else:
-				if info:
-					self.feedback_box.set_name("INFORMATION_BOX")
-					self.feedback_ok_img.hide()
-					self.feedback_error_img.hide()
-					self.feedback_info_img.show()
-				else:
-					self.feedback_box.set_name("SUCCESS_BOX")
-					self.feedback_ok_img.show()
-					self.feedback_error_img.hide()
-					self.feedback_info_img.hide()
-
-	#def manage_feedback_box
-
+	'''
 #class EpiBox
 
 from . import Core
